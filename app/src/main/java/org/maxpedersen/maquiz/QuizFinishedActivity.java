08f2bf5ad3a982d36af120828aeb7eaa241f5c63
Alex <z5164310@student.unsw.edu.au>
2019-04-22 22:51:09 +1000

    Adding finished quiz graphic

--- app/src/main/java/org/maxpedersen/maquiz/QuizFinishedActivity.java
+++ app/src/main/java/org/maxpedersen/maquiz/QuizFinishedActivity.java
@@ -2,6 +2,7 @@ package org.maxpedersen.maquiz;
 
 import android.arch.persistence.room.Room;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,13 +11,20 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.maquiz.R;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
 
 public class QuizFinishedActivity extends AppCompatActivity {
 
     TextView userMsg;
     TextView scoreTV;
     TextView XPTV;
-
+    PieChart pieChart;
     Button homeBtn;
 
     @Override
@@ -40,7 +48,7 @@ public class QuizFinishedActivity extends AppCompatActivity {
 
 
 
-
+        inflatePieChart(score);
         /* Approach #1- relying on Score being put into the User Table
         int existingScore = db.userDAO().getExistingScore((UserValueCapture.zIDGlobal));
 
@@ -82,5 +90,34 @@ public class QuizFinishedActivity extends AppCompatActivity {
     public void goHome(){
         Intent newIntent = new Intent(this, MainActivity.class);
         startActivity(newIntent);
+    }
+
+    public void inflatePieChart(int right){
+        int correct = right-1;
+        int wrong = 10-right;
+        pieChart = (PieChart) findViewById(R.id.pieChart);
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10,5,5);
+        pieChart.setDrawEntryLabels(false);
+        pieChart.getLegend().setEnabled(false);
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(android.R.color.white);
+        pieChart.setTransparentCircleRadius(61f);
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+        yValues.add(new PieEntry(correct, "Corect"));
+        yValues.add(new PieEntry(right, "Wrong"));
+        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+        PieDataSet dataSet = new PieDataSet(yValues, "Question");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(new int[]{
+                Color.parseColor("#008000"),
+                Color.parseColor("#21000000")
+        });
+        dataSet.setDrawValues(false);
+        PieData data = new PieData((dataSet));
+        pieChart.setData(data);
     }
 }
