3d2d0674aff8a145e62116361f25919158b740d5
Max <30624744+max-pedersen@users.noreply.github.com>
2019-04-26 18:07:00 +1000

    Fix API error message

--- app/src/main/java/org/maxpedersen/maquiz/NewsArticleService.java
+++ app/src/main/java/org/maxpedersen/maquiz/NewsArticleService.java
@@ -7,6 +7,6 @@ import retrofit2.Call;
 public interface NewsArticleService {
     //stores the API call
     //@GET("everything/?q=artificial+intelligence&language=en&from=2019-04-01&sources=abc-news-au,australian-financial-review,the-telegraph,the-new-york-times&sortBy=relevancy&apiKey=c06da6e3036740838a0730ef5899cbb0")
-    @GET("everything?q=artificial+intelligence&language=en&from=2019-04-01&sources=abc-news-au+australian-financial-review+the-telegraph+the-new-york-times&sortBy=relevancy&apiKey=c06da6e3036740838a0730ef5899cbb0")
+    @GET("everything?q=artificial+intelligence&language=en&from=2019-04-01&sources=abc-news-au,australian-financial-review&sortBy=publishedAt&apiKey=c06da6e3036740838a0730ef5899cbb0")
     Call<NewsArticleResponse> getRelevantArticles();
 }
